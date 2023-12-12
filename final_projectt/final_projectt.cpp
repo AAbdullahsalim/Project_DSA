8547ae7f8158a80812ea3a9f1a0ba3e1acc9fb2e
AAbdullahsalim <151334821+AAbdullahsalim@users.noreply.github.com>
2023-12-12 18:13:02 +0500

    Test Commit

--- final_projectt/final_projectt.cpp
+++ final_projectt/final_projectt.cpp
@@ -6,6 +6,9 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hello1 World!\n";
+    std::cout << "Hello2 World!\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
